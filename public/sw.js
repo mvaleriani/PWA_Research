b8dcbf53c2273f8e0adb59715729b3d423e77b9b
mvaleriani <michaelvaleriani@gmail.com>
2018-06-29 12:23:04 -0700

    add service worker

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+    console.log('[SW] Installing Service Worker ...', e);  
+});
+
+self.addEventListener('activate', e => {
+    console.log('[SW] Activating Service Worker ...', e);
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    console.log('[SW] Fetching something ...', e);
+    e.respondWith(fetch(e.request));
+});
