5e39e2131a1c8befe3856155d8618a3c15eedab1
mvaleriani <michaelvaleriani@gmail.com>
2018-07-01 21:35:01 -0700

    add listener to App

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,3 +5,11 @@ if ('serviceWorker' in navigator){
             console.log('Service Worker has been registered!');
         });
 }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+            console.log('Service Worker has been registered!');
+        });
+}
